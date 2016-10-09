04ff95bd06f1d4c24be48e8d7a54ecdf1468e7b4
'RevelationCollection' <'310002170@qq.com'>
2016-10-09 20:59:43 +0800

    添加注释

--- src/com/jbp/oracle/factory/DAOFactory.java
+++ src/com/jbp/oracle/factory/DAOFactory.java
@@ -2,11 +2,11 @@ package com.jbp.oracle.factory;
 
 public class DAOFactory {
 	private DAOFactory(){}
+	//DAO工厂类，私有化构造方法，减少不必要的实例化对象。
 	public static <T>T getInstance(Class<T> cls){
 		try {
 			return cls.newInstance();
 		} catch (InstantiationException | IllegalAccessException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
