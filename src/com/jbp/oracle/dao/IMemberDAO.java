1c9c6dfa2e4a38d5ddec2c69bc9db05c610cb0b7
'RevelationCollection' <'310002170@qq.com'>
2016-10-09 20:57:04 +0800

    添加注释

--- src/com/jbp/oracle/dao/IMemberDAO.java
+++ src/com/jbp/oracle/dao/IMemberDAO.java
@@ -3,5 +3,12 @@ package com.jbp.oracle.dao;
 import com.jbp.oracle.vo.Member;
 
 public interface IMemberDAO extends IDAO<String, Member> {
+	/**
+	 * 根据自己的需求扩充自己的特色方法
+	 * 根据电话查询Vo
+	 * @param Phone 
+	 * @return
+	 * @throws Exception
+	 */
 	public Member findByPhone(String Phone) throws Exception;
 }
