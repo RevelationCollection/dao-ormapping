504498cf2e841dba57c436b81daffcf33a6b6f20
'RevelationCollection' <'310002170@qq.com'>
2016-10-09 21:21:05 +0800

    获取配置文件信息

--- src/com/jbp/oracle/util/PrimaryKey.java
+++ src/com/jbp/oracle/util/PrimaryKey.java
@@ -5,6 +5,11 @@ import java.util.ResourceBundle;
 public class PrimaryKey {
 	private PrimaryKey(){
 	}
+	/**
+	 * 取得配置文件中的主键名称
+	 * @param className
+	 * @return
+	 */
 	public static String getPrmaryKey(String className){
 		return ResourceBundle.getBundle("Primary").getString(className);
 	}
