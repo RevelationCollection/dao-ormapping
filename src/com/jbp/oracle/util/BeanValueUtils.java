a714e333808edd7ce00662310ae537e7573fd49b
'RevelationCollection' <'310002170@qq.com'>
2016-10-09 21:17:39 +0800

    反射调用vo类中的set和get方法

--- src/com/jbp/oracle/util/BeanValueUtils.java
+++ src/com/jbp/oracle/util/BeanValueUtils.java
@@ -5,6 +5,13 @@ import java.lang.reflect.Method;
 public class BeanValueUtils {
 	private BeanValueUtils(){}
 	@SuppressWarnings("unchecked")
+	/**
+	 * 反射取得并调用get方法（一层）
+	 * @param obj 实例化对象
+	 * @param name 方法名称
+	 * @param cls 返回的类型
+	 * @return
+	 */
 	public static <T>T getMethod(Object obj,String name,Class<T> cls){
 		Method getMethod;
 		try {
@@ -15,6 +22,12 @@ public class BeanValueUtils {
 		}
 		return null;
 	}
+	/**
+	 * 反射调用set方法（一层）
+	 * @param obj
+	 * @param methodName
+	 * @param value
+	 */
 	public static void setMethod(Object obj,String methodName,Object value){
 		try {
 			Method setMethod = obj.getClass().getMethod("set"+StringUtils.initcap(methodName),
